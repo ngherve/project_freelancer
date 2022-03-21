dc49cca75d0167047114c995470751e0f55c0fb8
Seunghyun-H <100032754+Seunghyun-H@users.noreply.github.com>
2022-03-20 23:41:42 -0400

    documentation for test models

--- test/models/QueryTest.java
+++ test/models/QueryTest.java
@@ -4,8 +4,15 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+/**
+ * The type Query test.
+ * @author Seunghyun Hong
+ */
 public class QueryTest {
 
+    /**
+     * test Query constructor
+     */
     @Test
     public void queryTest() {
         Query query = new Query("query", "true");
--- test/models/SearchQueryStatsTest.java
+++ test/models/SearchQueryStatsTest.java
@@ -9,14 +9,36 @@ import models.Project;
 
 import static org.junit.Assert.*;
 
+/**
+ * Test class for Searchquerystats.
+ * @author Herve Ngomseu Fosting
+ */
 public class SearchQueryStatsTest {
 
+    /**
+     * Variable for test that contains skills
+     */
     ArrayList<Job> skills = new ArrayList<Job>(Arrays.asList(new Job("1", "java")));
+    /**
+     * Variable for test that project object
+     */
     Project proj = new Project("1", "123", 123456, "title", "type", "descriptions", skills);
+    /**
+     * Variable for test that contains projects
+     */
     List<Project> projects = new ArrayList<Project>(Arrays.asList(proj));
+    /**
+     * Variable for test that map have String and List of projects
+     */
     LinkedHashMap<String, List<Project>> projectMap = new LinkedHashMap<>();
+    /**
+     * Variable for test that contains search Query
+     */
     SearchQueryStats searchQS = new SearchQueryStats("java", projectMap);
 
+    /**
+     * test Searchquerystats constructor  method..
+     */
     @Test
     public void SearchQueryStats() {
         SearchQueryStats sqs = new SearchQueryStats("java", projectMap);
@@ -24,6 +46,9 @@ public class SearchQueryStatsTest {
     }
 
 
+    /**
+     * test Combinestrings  method..
+     */
     @Test
     public void combineStrings() {
         projectMap.put("name", projects);
@@ -31,6 +56,9 @@ public class SearchQueryStatsTest {
 
     }
 
+    /**
+     * test  Computewordlevelstat  method..
+     */
     @Test
     public void computeWordLevelStat() {
         projectMap.put("name", projects);
@@ -39,6 +67,9 @@ public class SearchQueryStatsTest {
         assertEquals(wordFreq,searchQS.computeWordLevelStat());
     }
 
+    /**
+     * test Computewordlevelstatbyproject method.
+     */
     @Test
     public void computeWordLevelStatByProject() {
         projectMap.put("name", projects);
@@ -48,6 +79,9 @@ public class SearchQueryStatsTest {
 
     }
 
+    /**
+     * test Combineoutput method.
+     */
     @Test
     public void combineOutput() {
         Map<String, Long> wordFreq = new HashMap<String, Long>() ;
