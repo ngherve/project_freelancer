258173fcb7ebd76f90e3cbf7bc9ce83c139f84ee
ngherve <217092052@student.uj.ac.za>
2022-04-13 14:49:03 -0400

    commenting module

--- app/modules/FreelancerModule.java
+++ app/modules/FreelancerModule.java
@@ -4,9 +4,17 @@ import com.google.inject.AbstractModule;
 import services.FreelancerApiService;
 import services.IApiService;
 
+/**
+ * Module for Freelancer class
+ * @author Seung Hyun Hong, Nastaran Naseri, Herve Ngomseu Fosting
+ */
 
 public class FreelancerModule  extends AbstractModule {
+
     @Override
+    /**
+     * configurig the binding from IAPIService to FreelancerApiService clas
+     */
     protected void configure() {
         bind(IApiService.class).to(FreelancerApiService.class);
     }
